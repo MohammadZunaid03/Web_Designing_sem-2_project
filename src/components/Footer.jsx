331856e334b5b29998ed03d16c8534e56d6f19a1
Mohammad Zunaid <corecompile03@gmail.com>
2026-06-11 01:02:56 +0530

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Footer.module.css'
-function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <p>Web Designing Workshop - II Project</p>
-    </footer>
-  )
-}
-export default Footer
